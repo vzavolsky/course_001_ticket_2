2573b00882cb0f92120b1e55508c7aa3f07e1bf9
vzavolsky <vzavolsky@ya.ru>
2023-03-08 22:21:42 +0300

    Exam - Course 1 - Ticket 2. Zavolsky V. Task solution - check for palindrome.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Course 1 - Ticket 2");
+        String palindrom = "loqaqol1";
+        int palindormLength = palindrom.length();
+        boolean flag = true;
+        for (int i = 0; i < palindormLength; i++, palindormLength--) {
+            if (palindrom.charAt(i) == palindrom.charAt(palindormLength - 1)) {
+                flag = true;
+            } else {
+                flag = false;
+            }
+        }
+        if (flag) {
+            System.out.println("Palindrom.");
+        } else {
+            System.out.println("Ne pelindorom.");
+        }
     }
 }
